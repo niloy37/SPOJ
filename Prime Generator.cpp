d9a19852d32ccb9ff6889f613faba12eb4b57636
Niloy Rahman <havocisgod@gmail.com>
2019-06-15 11:56:30 +0600

    Create Prime Generator.cpp

---
+++ Prime Generator.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main () {
+int n;
+long long int x,y;
+cin>>n;
+while(n--){
+    cin>>x>>y;
+    while(x<=y){
+            bool f = true;
+    for(int i=2;i<=sqrt(x);++i){
+        if(x%i == 0){
+            f = false;
+            break;
+        }
+
+
+    }     if(x==1){
+       }
+           else if(f==true){
+            cout<<x<<endl;
+        }
+        ++x;
+    }
+
+}
+
+}
